b7253ceec6649e05da527e4d97fd05e197c0db8a
tucond <kikkikukkuku@gmail.com>
2022-11-02 21:22:48 +0900

    Create main page

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,15 +2,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home({videoObj}) {
 
-  
-  let src = "https://www.youtube.com/embed/" + videoObj.videoId;
-  
+  let src = "https://www.youtube.com/embed/" + videoObj.videoId + "?" + "autoplay=1" + "&" + "mute=1";
+  console.log(src)
   return (
-    <div className="youtube">
-      <iframe src={src} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+    <>
+    <h1 className={styles.title}>Random YouTube Player</h1>
+    
+    <div className={styles.buttonStyle}>
+      <button onClick={()=>location.reload()}>REGENERATE</button>
     </div>
+
+    <div className={styles.youtube}> 
+      <iframe src={src} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    </div>
+
+
+    </>
   )
 }
 
